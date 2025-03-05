8834cfd06618ee408a698854f78cd69320e78fd9
Mutiu Oseni <mutiu.oseni@fintraksoftware.com>
2025-03-05 19:41:25 +0100

    #

--- SmartParkingSystem/obj/Debug/net8.0/SmartParkingSystem.AssemblyInfo.cs
+++ SmartParkingSystem/obj/Debug/net8.0/SmartParkingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartParkingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e755ba9528e5226cbf727c053d4e7f686cf63d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349730f779f9fe826d33d43705fbc2ab65cd9155")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartParkingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartParkingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
