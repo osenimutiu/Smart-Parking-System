a6fc07c6d3dd1e2bd2b825b372042c0002bd8ca4
Christian Obinna Anichebe <95555393+Christiancode-beep@users.noreply.github.com>
2025-03-11 12:01:37 +0100

    Update IParkingSpaceRepository.cs
    IParkingSpace Conflict Resolution

--- SmartParkingSystem/Contracts/IParkingSpaceRepository.cs
+++ SmartParkingSystem/Contracts/IParkingSpaceRepository.cs
@@ -4,11 +4,7 @@ namespace SmartParkingSystem.Contracts
 {
     public interface IParkingSpaceRepository
     {
-<<<<<<< HEAD
         Task<List<ParkingSpace>> GetListParkingSpaces(string role, string email);
-=======
-        Task<List<ParkingSpace>> GetListParkingSpaces();
->>>>>>> origin/master
         Task<ParkingSpace> GetParkingSpace(int id);
         Task DeleteParkingSpace(ParkingSpace ParkingSpace);
         Task<ParkingSpace> AddParkingSpace(ParkingSpace ParkingSpace);
