62e3321e50787e5c9a423c3722790a7d46ed9789
Christian Obinna Anichebe <95555393+Christiancode-beep@users.noreply.github.com>
2025-03-11 12:03:13 +0100

    Update IPaymentRepository.cs
    IPayment Conflict

--- SmartParkingSystem/Contracts/IPaymentRepository.cs
+++ SmartParkingSystem/Contracts/IPaymentRepository.cs
@@ -5,13 +5,8 @@ namespace SmartParkingSystem.Contracts
 {
     public interface IPaymentRepository
     {
-<<<<<<< HEAD
-        Task<Payment> ProcessPayment(BookingDto bookingDto);
-=======
         Task<InitiateResponse> InitializePayment(PaystackInitateRequest request);
         Task<PaystackVerifyResponse> VerifyPayment(string reference);
         Task<List<Payment>> GetSlothOwnersPaymentHistory(PaymentHistoryQueryParameters request);
-
->>>>>>> origin/master
     }
 }
