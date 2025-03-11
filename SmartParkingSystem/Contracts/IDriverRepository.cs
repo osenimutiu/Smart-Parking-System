90bc4037790e23d9452c35f01641d9d0c8413717
Christian Obinna Anichebe <95555393+Christiancode-beep@users.noreply.github.com>
2025-03-11 11:55:45 +0100

    Update IDriverRepository.cs
    IDriver Conflict

--- SmartParkingSystem/Contracts/IDriverRepository.cs
+++ SmartParkingSystem/Contracts/IDriverRepository.cs
@@ -1,21 +1,14 @@
 ﻿using SmartParkingSystem.Entities.Models;
-<<<<<<< HEAD
 using System.Data;
-=======
->>>>>>> origin/master
 
 namespace SmartParkingSystem.Contracts
 {
     public interface IDriverRepository
     {
         Task<List<Driver>> GetListDrivers();
-<<<<<<< HEAD
         Task<List<Driver>> GetDriversBasedOnUser(string role, string email);
         Task<Driver> GetDriver(int id);
         Task<Driver> GetDriverByEmail(string emailAddress);
-=======
-        Task<Driver> GetDriver(int id);
->>>>>>> origin/master
         Task DeleteDriver(Driver Driver);
         Task<Driver> AddDriver(Driver Driver);
         Task UpdateDriver(Driver Driver);
